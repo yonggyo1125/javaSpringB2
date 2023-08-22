d322d0bd2aa677c0c3a88330c8d84c678f40d25b
yonggyo1125 <yonggyo00@kakao.com>
2023-08-22 17:52:24 +0900

    .

--- 1. 화면구현/project/js/main.js
+++ 1. 화면구현/project/js/main.js
@@ -9,4 +9,22 @@ window.addEventListener("DOMContentLoaded", function() {
           el: ".swiper-pagination",
         },
     });
+
+    /* 더보기 메뉴 클릭시 */
+    const moreMenus = document.getElementsByClassName("more_menu");
+    const slideMenu = document.querySelector(".slide_menu");
+    const layerDim = document.querySelector(".layer_dim");
+    const closeBtn = document.querySelector(".slide_menu .close");
+
+    for (const el of moreMenus) {
+        el.addEventListener("click", function() {
+            
+        });
+    }
+
+    /* 닫기 버튼 클릭시 */
+    closeBtn.addEventListener("click", function() {
+
+    });
+
 });
