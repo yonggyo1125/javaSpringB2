63213afffe7fa2f9fee02136b472790fa6978f01
yonggyo1125 <yonggyo00@kakao.com>
2023-09-12 15:00:23 +0900

    수업 내용

---
+++ 4. 프로그래밍 언어 응용/java_study/day06/src/exam01/Calculator.java
@@ -0,0 +1,12 @@
+package exam01;
+
+ public abstract class Calculator {
+     int num = 10;
+
+     public abstract int add(int num1, int num2);
+     public abstract int minus(int num1, int num2);
+
+     public void commonMethod() {
+         System.out.println("공통 메서드!");
+     }
+}
---
+++ 4. 프로그래밍 언어 응용/java_study/day06/src/exam01/ComplexCalculator.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class ComplexCalculator extends Calculator {
+    @Override
+    public int add(int num1, int num2) {
+        return 0;
+    }
+
+    @Override
+    public int minus(int num1, int num2) {
+        return 0;
+    }
+}
