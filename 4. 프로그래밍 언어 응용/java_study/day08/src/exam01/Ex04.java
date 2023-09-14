3f4f5ec8d021e233516dfac6c41a6500b644137a
yonggyo1125 <yonggyo00@kakao.com>
2023-09-14 14:54:37 +0900

    수업 소스

--- 4. 프로그래밍 언어 응용/java_study/day08/src/exam01/Ex04.java
+++ 4. 프로그래밍 언어 응용/java_study/day08/src/exam01/Ex04.java
@@ -1,4 +1,13 @@
 package exam01;
 
 public class Ex04 {
+    public static void main(String[] args) {
+        LoginService service = new LoginService();
+        try {
+            service.login("user01", "12345");
+
+        } catch (UserIdException | UserPwException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- 4. 프로그래밍 언어 응용/java_study/day08/src/exam01/LoginService.java
+++ 4. 프로그래밍 언어 응용/java_study/day08/src/exam01/LoginService.java
@@ -1,25 +1,23 @@
 package exam01;
 
 public class LoginService {
-    public void login(String userId, String userPw) {
+    public void login(String userId, String userPw) throws UserIdException, UserPwException {
         /**
          * userId - user01
          * userPw - 1234
          */
-        try {
-            if (!userId.equals("user01")) {
-                throw new UserIdException("가입되지 않은 회원입니다.");
-            }
 
-            if (!userPw.equals("1234")) {
-                throw new UserPwException("비밀번호가 일치하지 않습니다.");
-            }
-
-            // 아이디와 비번이 전부 일치하는 경우
-            System.out.println("로그인 완료!");
-
-        } catch (UserIdException | UserPwException e) {
-            System.out.println(e.getMessage());
+        if (!userId.equals("user01")) {
+            throw new UserIdException("가입되지 않은 회원입니다.");
         }
+
+        if (!userPw.equals("1234")) {
+            throw new UserPwException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 아이디와 비번이 전부 일치하는 경우
+        System.out.println("로그인 완료!");
+
+
     }
 }
