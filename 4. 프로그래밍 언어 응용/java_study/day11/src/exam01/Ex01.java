3db14a378485291634543eff986c10147f10985f
yonggyo1125 <yonggyo00@kakao.com>
2023-09-19 15:30:31 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어 응용/java_study/day11/src/exam01/Ex01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+//@MyAnno
+public class Ex01 {
+
+    //@MyAnno
+    private String name;
+
+   ///@MyAnno
+    public void method() {
+
+    }
+}
---
+++ 4. 프로그래밍 언어 응용/java_study/day11/src/exam01/MyAnno.java
@@ -0,0 +1,13 @@
+package exam01;
+
+
+import static java.lang.annotation.ElementType.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({TYPE, METHOD, FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnno {
+    String value();
+}
