95935402bb81f597b1c3f76d4497c435071bab31
yonggyo1125 <yonggyo00@kakao.com>
2023-09-20 17:46:58 +0900

    수업소스

--- 4. 프로그래밍 언어 응용/java_study/day12/src/exam02/Ex03.java
+++ 4. 프로그래밍 언어 응용/java_study/day12/src/exam02/Ex03.java
@@ -1,12 +1,12 @@
 package exam02;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
-public class Ex02 {
+public class Ex03 {
     public static void main(String[] args) {
-        Set<Book> books = new HashSet<>();
+        List<Book> books = new ArrayList<>();
         books.add(new Book("책1", "저자1", "출판사1"));
         books.add(new Book("책2", "저자2", "출판사2"));
         books.add(new Book("책2", "저자2", "출판사2"));
@@ -14,22 +14,20 @@ public class Ex02 {
         books.add(new Book("책4", "저자4", "출판사4"));
         books.add(new Book("책5", "저자5", "출판사5"));
 
-        Iterator<Book> iter = books.iterator();
+        ListIterator<Book> iter = books.listIterator();
+
         while(iter.hasNext()) {
+            int num = iter.nextIndex();
             Book book = iter.next();
-            System.out.println(book);
+
+            System.out.printf("index : %d, %s%n", num, book);
         }
 
-        iter = books.iterator();
-        while(iter.hasNext()) {
-            Book book = iter.next();
-            System.out.println(book);
-        }
+        while(iter.hasPrevious()) {
+            int num = iter.previousIndex();
+            Book book = iter.previous();
 
-        /*
-        for (Book book : books) {
-            System.out.println(book);
+            System.out.printf("index : %d, %s%n", num, book);
         }
-         */
     }
 }
