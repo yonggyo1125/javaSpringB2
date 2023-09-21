1edd4173fd1a028390ebac5719da83ac753b26ba
yonggyo1125 <yonggyo00@kakao.com>
2023-09-21 15:19:24 +0900

    수업 소스

---
+++ 4. 프로그래밍 언어 응용/java_study/day13/src/exam01/Ex02.java
@@ -0,0 +1,33 @@
+package exam01;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Map<String, String> members = new HashMap<>();
+        members.put("user01", "사용자01");
+        members.put("user02", "사용자02");
+        members.put("user03", "사용자03");
+        members.put("user04", "사용자04");
+        members.put("user05", "사용자05");
+
+        members.put("user03", "(수정)사용자03");
+        members.remove("user03");
+        String name = members.get("user03");
+        System.out.println(name);
+        
+        String name2 = members.getOrDefault("user06", "없는 사용자");
+        System.out.println(name2);
+
+
+        Set<Map.Entry<String, String>> entries = members.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            System.out.printf("key=%s, value=%s%n", key, value);
+        }
+    }
+}
