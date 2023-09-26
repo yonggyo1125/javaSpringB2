d0b2993c2a4a8fef0c125d3ac916396c81050c17
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 14:18:03 +0900

    수업 소스

--- 4. 프로그래밍 언어 응용/java_study/day16/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam01/Ex01.java
@@ -1,4 +1,24 @@
 package exam01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Ex01 {
+    private static List<Student> students = new ArrayList<>();
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 20).forEach(Ex01::add);
+        students.stream().forEach(System.out::println);
+
+    }
+
+    private static void add(int i) {
+        int grade = (int)(Math.random() * 3) + 1;
+        int ban = (int)(Math.random() * 3) + 1;
+        char gender = (new Random()).nextBoolean() ? 'F':'M';
+
+        students.add(new Student(grade, ban, gender, "학생" + i));
+    }
+
 }
