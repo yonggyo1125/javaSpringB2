84354c500613777ae827ed451e87f697e77d5734
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 14:51:23 +0900

    수업 소스

--- 4. 프로그래밍 언어 응용/java_study/day16/src/exam01/Ex04.java
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam01/Ex04.java
@@ -7,23 +7,20 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Ex03 {
+public class Ex04 {
     private static List<Student> students = new ArrayList<>();
-
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 20).forEach(Ex03::add);
-        Map<Boolean, List<Student>> students1 = students.stream()
-                    .collect(Collectors.partitioningBy(s -> s.getGender() == 'F'));
+        IntStream.rangeClosed(1, 20).forEach(Ex04::add);
 
-        List<Student> females = students1.get(true);
-        List<Student> males = students1.get(false);
+        Map<String, List<Student>> students2 = students.stream()
+                .collect(Collectors.groupingBy(s -> s.getGrade() +"_" + s.getBan()));
+        System.out.println("--- 1학년 3반 ----");
+        List<Student> students3 = students2.get("1_3");
+        students3.stream().forEach(System.out::println);
 
-        System.out.println("---- Female ------");
-        females.stream().forEach(System.out::println);
-
-        System.out.println("---- Male -----");
-        males.stream().forEach(System.out::println);
-
+        System.out.println("--- 2학년 2반 ----");
+        List<Student> students4 = students2.get("2_2");
+        students4.stream().forEach(System.out::println);
     }
 
     private static void add(int i) {
