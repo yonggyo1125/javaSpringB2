0aca64be3c218ac5f5b118c4a5f5a7b31865ea39
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 15:51:00 +0900

    수업 소스

--- 4. 프로그래밍 언어 응용/java_study/day16/src/exam02/Ex03.java
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam02/Ex03.java
@@ -9,8 +9,14 @@ public class Ex03 {
             byte[] buffer = new byte[5];
             while(fis.available() > 0) {
                 int ch = fis.read(buffer);
+                /*
+                //System.out.println("바이트 수 : " + ch);
                 for (byte b : buffer) {
                     System.out.print((char)b);
+                }
+                 */
+                for (int i = 0; i < ch; i++) {
+                    System.out.print((char)buffer[i]);
                 }
                 System.out.println();
             }
