2246115ba052144dcc2be7846910d278d9581448
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 17:06:49 +0900

    수업 소스

--- 4. 프로그래밍 언어 응용/java_study/day16/src/exam03/ImplCacluator.java
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam03/ImplCacluator.java
@@ -3,11 +3,17 @@ package exam03;
 public class ImplCacluator implements Calculator {
     @Override
     public long factorial(long num) {
-        long total = 1L;
-        for (long i = 1L; i <= num; i++) {
-            total *= i;
-        }
+        long stime = System.currentTimeMillis();
+        try {
+            long total = 1L;
+            for (long i = 1L; i <= num; i++) {
+                total *= i;
+            }
 
-        return total;
+            return total;
+        } finally {
+            long etime = System.currentTimeMillis();
+            System.out.printf("걸린 시간 : %d%n", etime - stime);
+        }
     }
 }
--- 4. 프로그래밍 언어 응용/java_study/day16/src/exam03/RecCalculator.java
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam03/RecCalculator.java
@@ -3,8 +3,15 @@ package exam03;
 public class RecCalculator implements Calculator {
     @Override
     public long factorial(long num) {
-        if (num < 1) num = 1;
+        long stime = System.currentTimeMillis();
+        try {
+            if (num < 1)
+                return 1;
 
-        return num * factorial(num - 1);
+            return num * factorial(num - 1);
+        } finally {
+            long etime = System.currentTimeMillis();
+            System.out.printf("걸린 시간 : %d%n", etime - stime);
+        }
     }
 }
