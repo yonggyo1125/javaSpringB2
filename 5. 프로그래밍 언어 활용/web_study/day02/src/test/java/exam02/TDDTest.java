b5ee95135090bcc4f52977dfa8ea4b8595401d45
yonggyo1125 <yonggyo00@kakao.com>
2023-10-10 17:52:05 +0900

    수업 소스

--- 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/exam02/TDDTest.java
+++ 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/exam02/TDDTest.java
@@ -1,11 +1,35 @@
 package exam02;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class TDDTest {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach");
+    }
 
     @Test
     void test1() {
+        System.out.println("test1");
+    }
 
+    @Test
+    void Test2() {
+        System.out.println("test2");
     }
 }
--- 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/tests/JoinServiceTest.java
+++ 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/tests/JoinServiceTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import models.member.BadRequestException;
 import models.member.JoinService;
+import models.member.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,18 +11,44 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("회원가입 기능 테스트")
 public class JoinServiceTest {
 
+    private JoinService service;
+
+
+    private Member getMember() {
+        Member member = new Member();
+        member.setUserId("user" + System.currentTimeMillis());
+        member.setUserNm("사용자");
+        member.setUserPw("12345678");
+        member.setConfirmUserPw(member.getUserPw());
+
+        return member;
+    }
+
+    @BeforeEach
+    void init() {
+        service = new JoinService();
+    }
+
+
     @Test
     @DisplayName("회원가입 성공시 예외가 발생하지 않음")
     void joinSucess() {
         assertDoesNotThrow(() -> {
-            JoinService service = new JoinService();
-            service.join();
+            service.join(getMember());
         });
     }
     
     @Test
     @DisplayName("필수 항목(userId, userPw, confirmUserPw, userNm) 검증, 검증 실패시 BadRequestException 발생")
     void requiredFields() {
-        
+        // userId가 null, 또는 " "(빈 값)일때
+        assertThrows(BadRequestException.class, () -> {
+            Member member = getMember();
+            member.setUserId(null);
+            service.join(member);
+
+            member.setUserId("   ");
+            service.join(member);
+        });
     }
 }
