d13a0b07b26da4c711a14f7f670ef56aefd30d24
yonggyo1125 <yonggyo00@kakao.com>
2023-10-19 14:17:30 +0900

    수업 소스

--- 5. 프로그래밍 언어 활용/web_study/day05/src/main/java/controllers/members/LoginController.java
+++ 5. 프로그래밍 언어 활용/web_study/day05/src/main/java/controllers/members/LoginController.java
@@ -8,12 +8,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/member/login")
 public class LoginController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String[] addCss = {"member/style.css", "file/style.css"};
+        req.setAttribute("addCss", addCss);
+
+        List<String> addScript = Arrays.asList("file/fileupload.js", "member/form.js");
+        req.setAttribute("addScript", addScript);
 
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/login.jsp");
         rd.forward(req, resp);
