c37dcd0efd3effb87e17d92738b85d8ee97753a8
yonggyo1125 <yonggyo00@kakao.com>
2023-10-24 15:30:11 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/models/member/DuplicateMemberException.java
@@ -0,0 +1,7 @@
+package exam02.models.member;
+
+public class DuplicateMemberException extends RuntimeException {
+    public DuplicateMemberException() {
+        super("이미 가입된 회원 입니다.");
+    }
+}
