875144217838d873d3e6e06b435575900f6761d1
yonggyo1125 <yonggyo00@kakao.com>
2023-10-25 16:53:06 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam04/config/AppCtx.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam04/config/AppCtx.java
@@ -7,8 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
+/*
 @ComponentScan(basePackages = "exam04.models",
     excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=ManualBean.class))
+
+ */
+@ComponentScan(basePackages = "exam04.models",
+    excludeFilters = @ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE, classes=MemberDao.class))
 public class AppCtx {
     /*
     @Bean
--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam04/models/member/MemberDao.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam04/models/member/MemberDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-@ManualBean
+//@ManualBean
 public class MemberDao {
     private static Map<String, Member> members = new HashMap<>();
 
