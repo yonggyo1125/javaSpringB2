9e135f7b3ef9856559729b4b82fa22d383029ffc
yonggyo1125 <yonggyo00@kakao.com>
2023-10-27 17:51:47 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/exam01/Ex04.java
@@ -0,0 +1,21 @@
+package exam01;
+
+import config.AppCtx;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        JdbcTemplate jdbcTemplate = ctx.getBean(JdbcTemplate.class);
+        String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, EMAIL) " +
+                " VALUES (SEQ_MEMBER.nextval, ?, ?, ?, ?)";
+
+        int affectedRows = jdbcTemplate.update(sql, "user01", "123456", "사용자01", "user01@test.org");
+
+        System.out.println(affectedRows);
+
+        ctx.close();
+    }
+}
