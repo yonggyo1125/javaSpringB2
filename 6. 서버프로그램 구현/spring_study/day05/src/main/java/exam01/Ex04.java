c4437c14947075039b5ef8ea829e1120768e20e6
yonggyo1125 <yonggyo00@kakao.com>
2023-10-30 14:26:03 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/exam01/Ex04.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/exam01/Ex04.java
@@ -12,7 +12,7 @@ public class Ex04 {
         String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, EMAIL) " +
                 " VALUES (SEQ_MEMBER.nextval, ?, ?, ?, ?)";
 
-        int affectedRows = jdbcTemplate.update(sql, "user01", "123456", "사용자01", "user01@test.org");
+        int affectedRows = jdbcTemplate.update(sql, "user03", "123456", "사용자03", "user03@test.org");
 
         System.out.println(affectedRows);
 
