527e8f9de65bf56074d65d3771eb843d8fbec098
yonggyo1125 <yonggyo00@kakao.com>
2023-10-31 15:51:12 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/member/MemberController.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/member/MemberController.java
@@ -1,14 +1,30 @@
 package controllers.member;
 
-import controllers.RequestLogin;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+    /*
+    @Autowired
+    private HttpServletRequest request;
+
     @GetMapping("/member/login")
-    public String login(RequestLogin form) {
+    public String login(RequestLogin form, HttpServletResponse response) {
         System.out.println(form);
+        System.out.println(response);
+        System.out.println(request.getParameter("userId"));
+        return "member/login";
+    }
+     */
+
+    @GetMapping("/member/login")
+    public String login(RequestLogin form, Model model) {
+        model.addAttribute("message", "안녕하세요.");
 
         return "member/login";
     }
