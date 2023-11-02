c800d91013af7e176ba3b1a235f722a4db0603cc
yonggyo1125 <yonggyo00@kakao.com>
2023-11-02 16:50:41 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/member/JoinValidator.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/member/JoinValidator.java
@@ -2,6 +2,7 @@ package controllers.member;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
@@ -14,31 +15,10 @@ public class JoinValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         RequestJoin form = (RequestJoin)target;
-        // 필수 항목 검증(userId, userPw, confirmUserPw, userNm, email)
-        String userId = form.getUserId();
-        String userPw = form.getUserPw();
-        String confirmUserPw = form.getConfirmUserPw();
-        String userNm = form.getUserNm();
-        String email = form.getEmail();
-
-        if (userId == null || userId.isBlank()) {
-            errors.rejectValue("userId", "Required");
-        }
-
-        if (userPw == null || userPw.isBlank()) {
-            errors.rejectValue("userPw", "Required", "비밀번호를 입력하세요.");
-        }
-
-        if (confirmUserPw == null || confirmUserPw.isBlank()) {
-            errors.rejectValue("confirmUserPw", "Required", "비밀번호를 확인하세요.");
-        }
-
-        if (userNm == null || userNm.isBlank()) {
-            errors.rejectValue("userNm", "Required", "회원명을 입력하세요.");
-        }
-
-        if (email == null || email.isBlank()) {
-            errors.rejectValue("email", "Required", "이메일을 입력하세요.");
-        }
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userPw", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmUserPw", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userNm", "Required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Required");
     }
 }
