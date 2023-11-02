1c929d97ad99a174ba34fd188a8b56d2f336898f
yonggyo1125 <yonggyo00@kakao.com>
2023-11-02 17:54:02 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/commons/MobileValidator.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/commons/MobileValidator.java
@@ -2,7 +2,18 @@ package commons;
 
 public interface MobileValidator {
     default boolean checkMobile(String num) {
+        /**
+         * 010 0000 0000
+         * 011 000 0000
+         * 016 000 0000
+         *
+         * 01000000000 / 010 0000 0000 010-0000-0000, 010.0000.0000
+         *
+         * 000010000000001233
+         */
+        String mobile = num.replaceAll("\\D", "");
+        String pattern = "^01[016]\\d{3,4}\\d{4}$";
 
-        return false;
+        return mobile.matches(pattern);
     }
 }
