7e689628edf9f584c2cd30600bd7f368edb78f14
yonggyo1125 <yonggyo00@kakao.com>
2023-11-06 15:25:10 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/admin/MemberController.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/admin/MemberController.java
@@ -1,9 +1,15 @@
 package controllers.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("adminMemberController")
 @RequestMapping("/admin/member")
 public class MemberController {
+
+    @GetMapping
+    public String list() {
+        return "admin/member/list";
+    }
 }
