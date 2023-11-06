862a516761c1ef4b7c6acd647771daeb1f6c641e
yonggyo1125 <yonggyo00@kakao.com>
2023-11-06 17:19:20 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/file/FileController.java
@@ -0,0 +1,21 @@
+package controllers.file;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file/upload")
+public class FileController {
+
+    @GetMapping
+    public String upload() {
+        return "file/upload";
+    }
+
+    @PostMapping
+    public String uploadPs() {
+        return "file/upload";
+    }
+}
