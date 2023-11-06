95fc0cecb2b13ec5063f9995c91dfe2ff43aea21
yonggyo1125 <yonggyo00@kakao.com>
2023-11-06 17:54:08 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/file/FileController.java
+++ 6. 서버프로그램 구현/spring_study/day05/src/main/java/controllers/file/FileController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/file/upload")
@@ -15,7 +19,15 @@ public class FileController {
     }
 
     @PostMapping
-    public String uploadPs() {
+    public String uploadPs(MultipartFile file) {
+        File path = new File("C:/uploads/" + file.getOriginalFilename());
+
+        try {
+            file.transferTo(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return "file/upload";
     }
 }
